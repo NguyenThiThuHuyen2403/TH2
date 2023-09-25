90d48657c53b1ec8ac5c7b5e24e5bcfb5f1b1e25
unknown <huyend21cntt@gmail.com>
2023-09-25 15:25:31 +0700

    first commit

---
+++ SachOnline/SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SachOnline.Models;
+
+namespace SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+
+        public ActionResult SachBanNhieuPartial()
+        {
+            var listSachMoi = LaySachMoi(6);
+            return View(listSachMoi);
+        }
+        QLBanSachEntities data = new QLBanSachEntities();
+        
+        /// <summary> /// LaySachMoi /// </summary>
+        // <param name="count">int</param>
+        /// <returns>List</returns>
+        private List<SACH> LaySachMoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            //Lay 6 quyen sach moi
+            var listSachMoi = LaySachMoi(6); 
+            return View(listSachMoi);
+        }
+        public ActionResult ChuDePartial()
+        {
+            var listChuDe = from cd in data.CHUDEs select cd; 
+            return PartialView(listChuDe);
+        }
+        public ActionResult NhaXuatBanPartial()
+        {
+            var listNhaXuatBan = from cd in data.NHAXUATBANs select cd; 
+            return PartialView(listNhaXuatBan);
+        }
+    }
+
+
+}
+
+ 
